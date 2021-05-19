c3891cc01eb43995251baa15933f9bb6cd95cd0e
Maksaks <maksym.zhmutskyi@gmail.com>
2021-05-19 13:02:33 +0300

    First commit from Maksik

---
+++ SS_RGZ_2/SS_RGZ_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+    
+using namespace std;
+
+class Game {
+private:
+    int sizeX, sizeY;
+    bool** map;
+public:
+    void CoutMap(); // показ матрицы
+    void SetMap(); // установка значений матрицы
+    void DisplayMap(); // вывод игрового поля
+    void PlayGame(); // начало игры(логика)
+    void AllSetOne(); // все клетки живые
+    void SetRandom(); // рандомные значения
+    Game(int sizeX, int sizeY);
+    ~Game();
+};
+
+int main()
+{
+    system("color 70");
+    int x, y;
+    srand(time(NULL));
+    while (true) {
+        system("cls");
+        cout << "Input size x y" << endl;
+        cin >> x >> y;
+        if (x <= 0 || y <= 0) continue;
+        else break;
+    }
+
+   
+    
+
+    system("pause");
+    return 0;
+}
+
